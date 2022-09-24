159944cdadbb8c0d638ec413d64be6342a3b4973
TahaAkhlaq <takhlaq04@gmail.com>
2022-09-24 19:51:18 -0400

    Progress

--- Password Generator/PasswordGenerator.java
+++ Password Generator/PasswordGenerator.java
@@ -1,37 +1,47 @@
 import java.util.Scanner;
 
-public class PasswordGenerator {
-    public static void main(String[] args){
+class PasswordGenerator{
+public static void main(String[] args){ 
     
     Scanner scan = new Scanner(System.in);
-    
+
     String lower_cases = "abcdefghijklmnopqrstuvwxyz";
     String upper_cases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String symbols = "!@#$%^&*()?/";
 
     String password = "";
 
-    System.out.println("Please enter the number of characters long you would like your password to be.");
-    System.out.println("NOTE: It is strongly recommended to have a password that is at a minumum of 12 characers long.");
+    System.out.println("Please enter the number of passwords you would like to generate.");
+    int numberOfPasswords = scan.nextInt();
 
-    int digit = scan.nextInt();
-    12
-    for (int i = 0; i < digit; i++) {
-        int rand = (int)(3*Math.random());
+    System.out.println("Please enter the number of characters long you would like your password(s) to be.");
+    System.out.println("NOTE: It is strongly recommended to have a password that is at a minimum of 12 characters long.");
+    int charactersInPassword = scan.nextInt();
+
+    for (int i = 0; i < charactersInPassword; i++) {
+        int rand = (int)(3 * Math.random());
 
             switch(rand) {
                 case 0:
-                password += String.valueOf((int)(0 * Math.random()));
-                break;
-            case 1:
-                rand = (int)(lower_cases.length() * Math.random());
-                password += String.valueOf(lower_cases.charAt(rand));
-                break;
-            case 2:
-                rand = (int)(upper_cases.length()* Math.random());
-                password += String.valueOf(upper_cases.charAt(rand));
-                break;
+                    password += String.valueOf((int)(0 * Math.random()));
+                    break;
+
+                case 1:
+                    rand = (char)(lower_cases.length() * Math.random());
+                    password += String.valueOf(lower_cases.charAt(rand));
+                    break;
+
+                case 2:
+                    rand = (char)(upper_cases.length() * Math.random());
+                    password += String.valueOf(upper_cases.charAt(rand));
+                    break;
+
+                case 3:
+                    rand = (char)(symbols.length() * Math.random());
+                    password += String.valueOf(symbols.charAt(rand));
+                    break;
             }        
         }
-        System.out.println(password);
+    System.out.println(password);
     }
 }
