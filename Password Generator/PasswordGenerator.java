d70218a9134cc1f6a1b9e9d16f1ed69d2393901a
TahaAkhlaq <takhlaq04@gmail.com>
2022-09-24 22:33:51 -0400

    Progress

--- Password Generator/PasswordGenerator.java
+++ Password Generator/PasswordGenerator.java
@@ -5,43 +5,80 @@ public static void main(String[] args){
     
     Scanner scan = new Scanner(System.in);
 
-    String lower_cases = "abcdefghijklmnopqrstuvwxyz";
-    String upper_cases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    String symbols = "!@#$%^&*()?/";
+    // Declare Variables 
+    String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String symbols = "<>,.{}[]+=-_!@#$%^&*()?/";
 
     String password = "";
 
-    System.out.println("Please enter the number of passwords you would like to generate.");
+    //Text
+    System.out.println("Please enter the number of passwords you would like to generate: ");
     int numberOfPasswords = scan.nextInt();
 
-    System.out.println("Please enter the number of characters long you would like your password(s) to be.");
-    System.out.println("NOTE: It is strongly recommended to have a password that is at a minimum of 12 characters long.");
+    System.out.println("NOTE: It is Recommended that Passwords be at a Minimum of 16 Characters in Length.");
+    System.out.println("Supported Character Length by the Password Generator is 8-128. ");
+    System.out.println("Please Enter the Number of Characters you would like in your Password(s): ");
     int charactersInPassword = scan.nextInt();
 
-    for (int i = 0; i < charactersInPassword; i++) {
-        int rand = (int)(3 * Math.random());
+    if (charactersInPassword < 8){
+        System.out.println("Please Enter A Higher Character Length. ");
+        }
+    
+    if (charactersInPassword > 128){
+        System.out.println("Please Enter A Lower Character Length. ");
+        }
 
-            switch(rand) {
+    //Print Multiple Passwords and Correct Character Length
+        for (int i = 0; i < numberOfPasswords; i++) {
+        password ="";
+
+    
+            for (int j = 0; j < charactersInPassword; j++) {
+            int random = (int)(4 * Math.random());
+
+            switch(random) {
                 case 0:
-                    password += String.valueOf((int)(0 * Math.random()));
+                    password += String.valueOf((int)(10 * Math.random()));
                     break;
-
+            
                 case 1:
-                    rand = (char)(lower_cases.length() * Math.random());
-                    password += String.valueOf(lower_cases.charAt(rand));
+                    random = (int)(upperCase.length() * Math.random());
+                    password += String.valueOf(upperCase.charAt(random));
                     break;
 
                 case 2:
-                    rand = (char)(upper_cases.length() * Math.random());
-                    password += String.valueOf(upper_cases.charAt(rand));
+                    random = (int)(lowerCase.length() * Math.random());
+                    password += String.valueOf(lowerCase.charAt(random));
                     break;
 
                 case 3:
-                    rand = (char)(symbols.length() * Math.random());
-                    password += String.valueOf(symbols.charAt(rand));
+                    random = (int)(symbols.length() * Math.random());
+                    password += String.valueOf(symbols.charAt(random));
                     break;
-            }        
+                    
+                }
+            
+            }
+            System.out.println(password);
         }
-    System.out.println(password);
+
+    //Password Strength
+    if (charactersInPassword <= 7){
+        System.out.println("Password Strength: WEAK");
+        }
+
+    if (charactersInPassword >= 8 && charactersInPassword <= 11 ){
+        System.out.println("Password Strength: STRONG");
+        }
+
+    if (charactersInPassword >= 12 && charactersInPassword <= 15){
+         System.out.println("Password Strength: VERY STRONG");
+        }
+
+    if (charactersInPassword >= 16){
+        System.out.println("Password Strength: EXTREMELY STRONG");
+        } 
+    
     }
 }
