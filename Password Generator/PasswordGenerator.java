dc701252414cd2d0712bfc212d468004699a4243
TahaAkhlaq <takhlaq04@gmail.com>
2022-09-28 14:44:16 -0400

    Password Generator

--- Password Generator/PasswordGenerator.java
+++ Password Generator/PasswordGenerator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class PasswordGenerator {
+public class PasswordGenerator {
     public static void main(String[] args) {
 
         try (Scanner scan = new Scanner(System.in)) {
