a489752a32bf018abbc381019e6f6313aaf46c5c
TahaAkhlaq <takhlaq04@gmail.com>
2022-09-28 13:40:18 -0400

    Converts Minutes to 24 Hour Time

---
+++ TimeConversion/TimeConversion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TimeConversion {
+    public static void main(String[] args) {
+
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Enter the time in minutes: ");
+            int minutes = scan.nextInt();
+
+            int hours = minutes / 60;
+            int m = minutes % 60;
+
+            if (m < 10)
+                System.out.println("The time is " + hours + ":0" + m);
+            else
+                System.out.println("The time is " + hours + ":" + m);
+        }
+
+    }
+}
